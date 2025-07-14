735e905a49bcb16a5ffb87e81bba4b4f1ff18615
jeromeku <jerome.ku@gmail.com>
2025-07-14 13:43:40 -0700

    demonstrate difference between make_subclass and make_wrapper_subclass

--- tensor_subclass.py
+++ tensor_subclass.py
@@ -17,19 +17,29 @@ def _make(cls, data: torch.Tensor):
 
 class First(torch.Tensor):
     @staticmethod
-    def __new__(cls, data): return _make(cls, data)
+    def __new__(cls, data): 
+        self = torch.Tensor._make_subclass(cls, torch.empty_like(data))
+        self._data = data
+        return self
 
     @classmethod
     def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
         print("First handles", func.__name__)
         # fall through to the next candidate
-        with no_dispatch():
-            return func(*args, **(kwargs or {}))
+        # breakpoint()
+        if isinstance(args[0], cls):
+            new_args = (args[0]._data, *args[1:])
+            return func(*new_args, **(kwargs or {}))
 
 class Second(torch.Tensor):
     @staticmethod
-    def __new__(cls, data): return _make(cls, data)
+    def __new__(cls, data: torch.Tensor): 
+        self = torch.Tensor._make_wrapper_subclass(cls, data.size(), data.stride(), data.storage_offset())
+        return self
 
+    def __init__(self, data: torch.Tensor):
+        self._tensor = data
+        
     @classmethod
     def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
         print("Second handles", func.__name__)
@@ -42,5 +52,5 @@ b = Second(torch.eye(2))
 print("a @ b  -->")
 torch.matmul(a, b)       # First is left-most → First runs first
 
-print("\nb @ a  -->")
-torch.matmul(b, a)       # Second is left-most → Second runs first
+# print("\nb @ a  -->")
+# torch.matmul(b, a)       # Second is left-most → Second runs first
